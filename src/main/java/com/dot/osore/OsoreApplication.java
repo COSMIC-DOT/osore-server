b8543be76969802f2038c62e75d2968cc260140e
minseok-oh <minseok.dev@gmail.com>
2024-04-03 22:20:04 +0900

    feat: spring boot starter를 통한 프로젝트 생성

---
+++ src/main/java/com/dot/osore/OsoreApplication.java
@@ -0,0 +1,13 @@
+package com.dot.osore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsoreApplication.class, args);
+	}
+
+}
