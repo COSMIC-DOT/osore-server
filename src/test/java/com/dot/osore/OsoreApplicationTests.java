e69fd9724cff02a360fd453bfc2f659dfee68fca
MinSeok Oh <minseok.dev@gmail.com>
2024-04-03 22:25:51 +0900

    feat/init-setting -> main (#2)
    * feat: spring boot starter를 통한 프로젝트 생성
    
    * feat: github pr, issue template 생성
    
    * feat: spring boot 전용 dockerfile 생성

---
+++ src/test/java/com/dot/osore/OsoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dot.osore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
