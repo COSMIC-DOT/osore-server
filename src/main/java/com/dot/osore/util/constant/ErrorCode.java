ad8b34605d3031ee79682d109f1ffddaf4d87008
MinSeok Oh <minseok.dev@gmail.com>
2024-04-30 17:08:54 +0900

    feat/response -> main (#4)
    * chore: gradle lombok 의존성 추가
    
    * feat: response 관련 객체 코드 작성
    
    * feat: error code를 담는 enum 생성

---
+++ src/main/java/com/dot/osore/util/constant/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.dot.osore.util.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    MEMBER_NOT_FOUND_EXCEPTION(1, "사용자가 존재하지 않습니다.");
+
+    private final Integer code;
+    private final String message;
+}
