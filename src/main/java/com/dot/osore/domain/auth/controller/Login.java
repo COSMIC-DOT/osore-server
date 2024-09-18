184bf8017fd1cbf129d31575096e895335faddee
minseok-oh <minseok.dev@gmail.com>
2024-09-18 14:45:14 +0900

    feat: Login Annotation 및 ArgumentResolver 생성

---
+++ src/main/java/com/dot/osore/domain/auth/controller/Login.java
@@ -0,0 +1,11 @@
+package com.dot.osore.domain.auth.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = {ElementType.PARAMETER})
+public @interface Login {
+}
