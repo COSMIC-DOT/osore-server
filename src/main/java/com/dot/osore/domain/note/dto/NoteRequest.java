6209af8c1de6f442ae19806195080b4b0c8bcf8e
MinSeok Oh <minseok.dev@gmail.com>
2024-09-21 17:18:54 +0900

    refactor: 사용하는 dto를 record로 변경

--- src/main/java/com/dot/osore/domain/note/dto/NoteRequest.java
+++ src/main/java/com/dot/osore/domain/note/dto/NoteRequest.java
@@ -1,11 +1,9 @@
 package com.dot.osore.domain.note.dto;
 
-import lombok.Data;
-
-@Data
-public class NoteRequest {
-    private String url;
-    private String title;
-    private String branch;
-    private String version;
+public record NoteRequest(
+        String url,
+        String title,
+        String branch,
+        String version
+) {
 }
