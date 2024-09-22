8bed7ace4f6bf837403f0be08acaad03795d2573
MinSeok Oh <minseok.dev@gmail.com>
2024-09-22 11:03:03 +0900

    feat: file 목록 조회 기능 구현

--- src/main/java/com/dot/osore/core/file/dto/FileInfoResponse.java
+++ src/main/java/com/dot/osore/core/file/dto/FileInfoResponse.java
@@ -1,5 +1,6 @@
 package com.dot.osore.core.file.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record FileInfoResponse (
@@ -8,4 +9,13 @@ public record FileInfoResponse (
         String extension,
         List<FileInfoResponse> children
 ) {
+
+    public FileInfoResponse findChildByName(String name) {
+        for (FileInfoResponse child : children) {
+            if (child.name.equals(name)) {
+                return child;
+            }
+        }
+        return null;
+    }
 }
