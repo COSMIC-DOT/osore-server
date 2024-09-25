7c50656b98c730de83df176e25c9e685f909d063
MinSeok Oh <minseok.dev@gmail.com>
2024-09-25 09:25:23 +0900

    feat: DeatilFileInfo 구현 및 기존 FileInfo 이름 변경

--- src/main/java/com/dot/osore/core/file/dto/FileInfoListResponse.java
+++
@@ -1,8 +0,0 @@
-package com.dot.osore.core.file.dto;
-
-import java.util.List;
-
-public record FileInfoListResponse(
-        List<FileInfoResponse> fileInfo
-) {
-}
---
+++ src/main/java/com/dot/osore/core/file/dto/SimpleFileInfoResponse.java
@@ -0,0 +1,31 @@
+package com.dot.osore.core.file.dto;
+
+import java.util.TreeSet;
+
+public record SimpleFileInfoResponse(
+        String type,
+        String name,
+        String extension,
+        TreeSet<SimpleFileInfoResponse> children
+) implements Comparable<SimpleFileInfoResponse> {
+
+    public SimpleFileInfoResponse findChildByName(String name) {
+        for (SimpleFileInfoResponse child : children) {
+            if (child.name.equals(name)) {
+                return child;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int compareTo(SimpleFileInfoResponse simpleFileInfoResponse) {
+        if (this.type.equals("folder") && simpleFileInfoResponse.type.equals("file")) {
+            return -1;
+        }
+        if (this.type.equals("file") && simpleFileInfoResponse.type.equals("folder")) {
+            return 1;
+        }
+        return this.name.compareTo(simpleFileInfoResponse.name);
+    }
+}
