a0fcf8d114c21637abe96d044e3c6a05ac64f84b
MinSeok Oh <minseok.dev@gmail.com>
2024-09-30 13:08:44 +0900

    feat: 파일 정보 반환 시 확장자에 해당하는 언어를 함께 담아 반환

--- src/main/java/com/dot/osore/core/file/dto/DetailFileInfoResponse.java
+++ src/main/java/com/dot/osore/core/file/dto/DetailFileInfoResponse.java
@@ -1,14 +1,25 @@
 package com.dot.osore.core.file.dto;
 
 import com.dot.osore.core.file.entity.File;
+import com.dot.osore.global.resolver.LanguageResolver;
 
 public record DetailFileInfoResponse(
-        String content
+        String content,
+        String language
 ) {
 
     public static DetailFileInfoResponse from(File file) {
+        String part = file.getPath();
+        String extension;
+        if (part.contains(".")) {
+            extension = part.substring(part.lastIndexOf(".") + 1);
+        } else {
+            extension = part.substring(part.lastIndexOf("/") + 1).toLowerCase();
+        }
+
         return new DetailFileInfoResponse(
-                file.getContent()
+                file.getContent(),
+                LanguageResolver.getLanguageByExtension(extension)
         );
     }
 }
---
+++ src/main/java/com/dot/osore/global/resolver/LanguageResolver.java
@@ -0,0 +1,50 @@
+package com.dot.osore.global.resolver;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LanguageResolver {
+    private static final Map<String, String> extensionToLanguageMap = new HashMap<>();
+
+    static {
+        extensionToLanguageMap.put("js", "javascript");
+        extensionToLanguageMap.put("ts", "typescript");
+        extensionToLanguageMap.put("py", "python");
+        extensionToLanguageMap.put("java", "java");
+        extensionToLanguageMap.put("cpp", "cpp");
+        extensionToLanguageMap.put("c", "c");
+        extensionToLanguageMap.put("cs", "csharp");
+        extensionToLanguageMap.put("rb", "ruby");
+        extensionToLanguageMap.put("go", "go");
+        extensionToLanguageMap.put("php", "php");
+        extensionToLanguageMap.put("html", "html");
+        extensionToLanguageMap.put("css", "css");
+        extensionToLanguageMap.put("json", "json");
+        extensionToLanguageMap.put("xml", "xml");
+        extensionToLanguageMap.put("yaml", "yaml");
+        extensionToLanguageMap.put("md", "markdown");
+        extensionToLanguageMap.put("rs", "rust");
+        extensionToLanguageMap.put("swift", "swift");
+        extensionToLanguageMap.put("kt", "kotlin");
+        extensionToLanguageMap.put("sh", "shell");
+        extensionToLanguageMap.put("bat", "batchfile");
+        extensionToLanguageMap.put("dockerfile", "dockerfile");
+        extensionToLanguageMap.put("toml", "toml");
+        extensionToLanguageMap.put("ini", "ini");
+        extensionToLanguageMap.put("sql", "sql");
+        extensionToLanguageMap.put("r", "r");
+        extensionToLanguageMap.put("pl", "perl");
+        extensionToLanguageMap.put("dart", "dart");
+        extensionToLanguageMap.put("scala", "scala");
+        extensionToLanguageMap.put("lua", "lua");
+        extensionToLanguageMap.put("perl", "perl");
+        extensionToLanguageMap.put("vb", "vb");
+    }
+
+    public static String getLanguageByExtension(String extension) {
+        return extensionToLanguageMap.getOrDefault(extension.toLowerCase(), "plaintext");
+    }
+
+}
