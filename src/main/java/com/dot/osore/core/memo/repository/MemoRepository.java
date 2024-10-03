2cf4bfe0cec0d0a78af1bd698277d4614567aab5
MinSeok Oh <minseok.dev@gmail.com>
2024-10-03 18:26:20 +0900

    feat: 메모 리스트 반환 서비스 로직 구현

--- src/main/java/com/dot/osore/core/memo/repository/MemoRepository.java
+++ src/main/java/com/dot/osore/core/memo/repository/MemoRepository.java
@@ -1,9 +1,11 @@
 package com.dot.osore.core.memo.repository;
 
 import com.dot.osore.core.memo.entity.Memo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findByNoteId(Long noteId);
 }
