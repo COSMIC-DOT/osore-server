9ef503939f902750b74f6f3b0e9907abfe0cbf05
MinSeok Oh <minseok.dev@gmail.com>
2024-10-23 17:08:59 +0900

    feat: 채팅을 위한 로직 구현

--- src/main/java/com/dot/osore/core/chat/controller/ChatController.java
+++ src/main/java/com/dot/osore/core/chat/controller/ChatController.java
@@ -1,7 +1,11 @@
 package com.dot.osore.core.chat.controller;
 
+import com.dot.osore.core.auth.dto.SignInInfo;
+import com.dot.osore.core.auth.handler.Login;
+import com.dot.osore.core.chat.dto.ChattingRoomResponse;
 import com.dot.osore.core.chat.dto.CreateChattingRoomRequest;
 import com.dot.osore.core.chat.dto.CreateChattingRoomResponse;
+import com.dot.osore.core.chat.dto.SendChatRequest;
 import com.dot.osore.core.chat.service.ChatService;
 import com.dot.osore.global.constant.ErrorCode;
 import com.dot.osore.global.response.Response;
@@ -18,8 +22,18 @@ public class ChatController {
 
     private final ChatService chatService;
 
+    @PostMapping("/chat")
+    public Response sendChat(@RequestBody SendChatRequest request, @Login SignInInfo signInInfo) {
+        try {
+            chatService.sendChat(request.roomId(), request.chat());
+            return Response.success(chatService.getChattingRoomList(request.noteId()));
+        } catch (Exception e) {
+            return Response.failure(ErrorCode.MEMBER_NOT_FOUND_EXCEPTION);
+        }
+    }
+
     @PostMapping("/chat-room")
-    public Response createChatRoom(@RequestBody CreateChattingRoomRequest request) {
+    public Response createChatRoom(@RequestBody CreateChattingRoomRequest request, @Login SignInInfo signInInfo) {
         try {
             Long noteId = request.noteId();
             Long chatRoomId = chatService.createChatRoom(noteId);
