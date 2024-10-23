5b90db326f6715ce2c69bd3e51d56681c0ef4012
MinSeok Oh <minseok.dev@gmail.com>
2024-10-23 16:11:53 +0900

    feat: 채팅 관련 엔티티 구현
    - 채팅의 전송자를 표시하기 위해 Enum을 구현함

---
+++ src/main/java/com/dot/osore/core/chat/entity/Chat.java
@@ -0,0 +1,39 @@
+package com.dot.osore.core.chat.entity;
+
+import com.dot.osore.core.chat.constant.ChatSender;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "chat")
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private ChatSender sender;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatting_room_id")
+    private ChattingRoom chattingRoom;
+}
